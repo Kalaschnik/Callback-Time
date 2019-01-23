d217277a9531d69bc3c9f9ebbd0d768dbaf6a16b
Steven Kalinke <steven.kalinke@eccenca.com>
2019-01-23 14:06:43 +0100

    ESLint files and add some explanation

--- src/async-await.js
+++ src/async-await.js
@@ -1,57 +1,56 @@
 const posts = [
-    {title: "Post One", body: "This is the very first post in my new blog and I love to share some thoughts..."},
-    {title: "Post Two", body: "Second Post. Great isn’t it? Yet, I don’t know what to write now..."},
+  { title: 'Post One', body: 'This is the very first post in my new blog and I love to share some thoughts...' },
+  { title: 'Post Two', body: 'Second Post. Great isn’t it? Yet, I don’t know what to write now...' },
 ];
 
 function getPosts() {
-    // setTimeout(callback(){}, delayInMs);
-    setTimeout(() => {
-
-        let output = '';
-        posts.forEach(post => {
-            output += `<li>${post.title}</li>`;
-        });
-        console.log(output);
-        document.body.innerHTML = output;
-
-    }, 1000);
-
-}
-
-const createPost = post => {
-    // this returns a promise
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            posts.push(post);
-
-            const error = false;
-            if (!error) {
-                resolve();
-            } else {
-                reject("Error: Something went wrong!");
-            }
-
-        }, 2000);
-
+  // setTimeout(callback(){}, delayInMs);
+  setTimeout(() => {
+    let output = '';
+    posts.forEach((post) => {
+      output += `<li>${post.title}</li>`;
     });
-};
-
-
-// async / await
-async function init() {
-    await createPost({title: "Third Post", body: "This is now a Promise..."});
-
-    getPosts();
+    console.log(output);
+    document.body.innerHTML = output;
+  }, 1000);
 }
 
+// create a promise that has two paramters with new Promise
+const createPost = post => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    posts.push(post);
+
+    const error = false;
+    if (!error) {
+      resolve();
+    } else {
+      reject(Error, 'Error: Something went wrong!');
+    }
+  }, 2000);
+});
+
+
+// async / await is just another syntax for promises
+// a function needs to be labeld with async if you want to use await inside of it
+async function init() {
+  await createPost({ title: 'Third Post', body: 'This is now a Promise...' });
+
+  // there is no need to use .then
+  // getPosts will only be executed when createPost is done the order now matters
+  getPosts();
+}
+
+// start create post method inside init
 init();
 
-// with fetch
+
+// async/await with fetch (arrow stylee)
 const fetchUsers = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  // since fetch returns a HTTP response, we need to call the .json() method to extract the response
+  const data = await response.json();
 
-  const data = await res.json();
-
+  // it only will log when the data is there
   console.log(data);
 };
 
--- src/promises.js
+++ src/promises.js
@@ -1,53 +1,49 @@
 const posts = [
-    {title: "Post One", body: "This is the very first post in my new blog and I love to share some thoughts..."},
-    {title: "Post Two", body: "Second Post. Great isn’t it? Yet, I don’t know what to write now..."},
+  { title: 'Post One', body: 'This is the very first post in my new blog and I love to share some thoughts...' },
+  { title: 'Post Two', body: 'Second Post. Great isn’t it? Yet, I don’t know what to write now...' },
 ];
 
 function getPosts() {
-    // setTimeout(callback(){}, delayInMs);
-    setTimeout(() => {
-
-        let output = '';
-        posts.forEach(post => {
-            output += `<li>${post.title}</li>`;
-        });
-        console.log(output);
-        document.body.innerHTML = output;
-
-    }, 1000);
-
+  // setTimeout(callback(){}, delayInMs);
+  setTimeout(() => {
+    let output = '';
+    posts.forEach((post) => {
+      output += `<li>${post.title}</li>`;
+    });
+    console.log(output);
+    document.body.innerHTML = output;
+  }, 1000);
 }
 
-const createPost = (post) => {
-    // this returns a promise
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            posts.push(post);
+// create a promise that has two paramters with new Promise
+const createPost = post => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    posts.push(post);
 
-            const error = false;
-            if (!error) {
-                resolve();
-            } else {
-                reject("Error: Something went wrong!");
-            }
+    const error = true;
+    if (!error) {
+      resolve();
+    } else {
+      reject(Error, 'Error: Something went wrong!');
+    }
+  }, 2000);
+});
 
-        }, 2000);
+// start createPost method
+createPost({ title: 'Third Post', body: 'This is now a Promise...' })
+// since createPost is returning a Promise we can use .then/.catch syntax
+  .then(getPosts)
+  .catch(err => console.log(err)); // if error = true
 
-    });
-};
-
-createPost({title: "Third Post", body: "This is now a Promise..."})
-    .then(getPosts) // if error = false
-    .catch(err => console.log(err)); // if error = true
 
 // Promise.all
 
-const promise1 = Promise.resolve("Hello World");
+const promise1 = Promise.resolve('Hello World'); // short for new Promise(res)
 const promise2 = 10;
-const promise3 = new Promise(res => setTimeout(res, 2000, "Goodbye"));
+const promise3 = new Promise(res => setTimeout(res, 2000, 'Goodbye'));
 // fetch returns a promise, but you need to map it
 const promise4 = fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json());
+  .then(response => response.json());
 
 Promise.all([promise1, promise2, promise3, promise4])
-    .then(values => console.log(values));
+  .then(values => console.log(values));
