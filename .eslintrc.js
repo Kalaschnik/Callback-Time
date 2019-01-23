f59dfb10fb5b5eac310135fdd0aaba54c74aadf0
Steven Kalinke <steven.kalinke@eccenca.com>
2019-01-23 13:03:13 +0100

    Add npm package and ESLint support

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "rules": {
+        "no-console": 'off',
+    },
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+        "node": true
+      }
+      
+};
