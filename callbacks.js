dd2efbbd8cf4edca69e7497654fcd0c488567cb4
Steven Kalinke <steven.kalinke@eccenca.com>
2019-01-23 13:03:34 +0100

    Add help

--- callbacks.js
+++ callbacks.js
@@ -1,26 +1,26 @@
 const posts = [
-    {title: "Post One", body: "This is the very first post in my new blog and I love to share some thoughts..."},
-    {title: "Post Two", body: "Second Post. Great isn’t it? Yet, I don’t know what to write now..."},
+  { title: 'Post One', body: 'This is the very first post in my new blog and I love to share some thoughts...' },
+  { title: 'Post Two', body: 'Second Post. Great isn’t it? Yet, I don’t know what to write now...' },
 ];
 
-function getPosts() {
-    // setTimeout(callback(){}, delayInMs);
-    setTimeout(() => {
-
-        let output = '';
+const getPosts = () => {
+  // setTimeout(callback(){}, delayInMs);
+  setTimeout(() => {
+    let output = '';
     posts.forEach((post) => {
-
-    }, 1000);
-
-}
-
-const createPost = (post, callback) => {
-    setTimeout(() => {
-        posts.push(post);
-        callback();
-    }, 2000)
+      output += `<li>${post.title}</li>`;
+    });
+    console.log(output);
+    document.body.innerHTML = output;
+  }, 1000);
 };
 
+const createPost = (post, callback) => {
+  setTimeout(() => {
+    posts.push(post);
+    callback();
+  }, 2000);
+};
 
-createPost({title: "Post Three", body: "This is post 3"}, getPosts);
-
+// createPosts also forwards the getPosts binding
+createPost({ title: 'Post Three', body: 'This is post 3' }, getPosts);
